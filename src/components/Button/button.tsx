a2089ee4f2333277cee5abd6bf3bbd7b2535d5af
海波 <linhaibo@147.com>
2020-06-06 09:41:18 +0800

    Button组件

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,76 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+/*
+ButtonHTMLAttributes——是button标签的默认属性值，范性 <HTMLElement> 设置了其元素类型
+BaseButtonProps & ButtonHTMLAttributes<HTMLElement> 中的 & 是联合类型，表示符合这两个类型
+但是为了选择这两个属性中的一些属性，使用：Partial< a & b> 的形式
+*/
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互
+ * ### 引用方法
+ *
+ * ~~~js
+ * import { Button } from 'vikingship'
+ * ~~~
+ */
+//在这里替换成了ButtonProps，表示props的类型
+export const Button: FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    className,
+    disabled,
+    size,
+    children,
+    href,
+    ...restProps
+  } = props
+  // btn, btn-lg, btn-primary
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === 'link') && disabled
+  })
+  if (btnType === 'link' && href ) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: 'default'
+}
+
+export default Button;
